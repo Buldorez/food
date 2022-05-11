ea98cf37db23e4967d88a031a86ef185c0f55d12
NP-106 <w2@jijs.ru>
2022-05-11 16:45:44 +0500

    1

---
+++ app/src/main/java/com/example/foodie/Login.kt
@@ -0,0 +1,13 @@
+package com.example.foodie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun login(view: android.view.View) {}
+}
